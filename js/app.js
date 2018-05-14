1b325623f3cf6094b041c110f02b01bba1539661
Willford <willfordzhan@gmail.com>
2018-05-14 17:26:33 +0800

    begin refactoring code

--- js/app.js
+++ js/app.js
@@ -1,20 +1,99 @@
-var clickImg1 = document.getElementsByClassName("clicker1")[0];
-var clickImg2 = document.getElementsByClassName("clicker2")[0];
-var timeShower1 = document.getElementsByClassName("timeShower1")[0];
-var timeShower2 = document.getElementsByClassName("timeShower2")[0];
-var clickTimes1 = 0;
-var clickTimes2 = 0;
+$(function(){
+
+    var model = {
+        init: function() {
+            
+        },
+        
+    };
 
 
-timeShower1.innerText = clickTimes1;
-timeShower2.innerText = clickTimes2;
+    var octopus = {
+        
+        
+        init: function() {
+            model.init();
+            catView.init();
+            catListView.init();
+        }
+    };
 
-clickImg1.addEventListener('click', function () {
-    clickTimes1++;
-    timeShower1.innerText = clickTimes1;
+
+    var catView = {
+        init: function() {
+            
+            catView.render();
+        },
+        render: function(){
+            
+        }
+    };
+
+    var catListView = {
+        init: function() {
+
+            catListView.render();
+        },
+        render: function() {
+
+        }
+    };
+
+    octopus.init();
 });
 
-clickImg2.addEventListener('click', function () {
-    clickTimes2++;
-    timeShower2.innerText = clickTimes2;
-});
+
+
+
+
+
+
+
+
+
+
+
+
+/* document.body.style.background="white";
+var catClickTimes = [];
+
+
+var buttons = document.getElementsByClassName("buttons")[0];
+var cat = document.getElementsByClassName("cat")[0];
+
+for (let i = 0; i < 5; i++) {
+    var catButton = document.createElement('button');
+    catButton.innerText = "cat" + i;
+    catClickTimes.push(0);
+    buttons.appendChild(catButton);
+};
+
+// 让我们循环遍历数组中的所有数字
+for (var i = 0; i < catClickTimes.length; i++) {
+    
+    var catClickTime = catClickTimes[i];
+    var catButton = buttons.childNodes[i];
+    // 我们为这个数字创建一个DOM元素
+    var catElem = document.createElement('div');
+    var catNameElem = document.createElement('div');
+    var catClicker = document.createElement('div');
+    var catShower = document.createElement('div');
+    
+    catNameElem.innerHTML = '<p>'+ 'cat' + i +'</p>';
+    catClicker.innerHTML = '<img src="img/cat'+ i +'.jpg">';
+
+    catClicker.addEventListener('click', (function(catClickTimeCopy){
+        catClickTimeCopy++;
+        catShower.innerHTML = '<p>Click times: </p> <p class="timeShower">'+ catClickTimeCopy +'</p>';
+    })(catClickTime));
+
+    catElem.appendChild(catNameElem);
+    catElem.appendChild(catClicker);
+    catElem.appendChild(catShower);
+
+    catButton.addEventListener('click',(function(catElemCopy){
+        cat.innerHTML = catElemCopy.innerHTML;
+    })(catElem));
+ };
+
+*/
